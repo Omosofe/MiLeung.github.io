78bfe73fec52187011f7582be67e1e4400778c1e
MiLeung <michaeleleung>
2015-10-07 13:14:34 -0700

    Bootstrap test

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//animate scrolling
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -6,10 +7,23 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	        'scrollTop': $target.offset().top - 60
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
 });
 
+//animate dropdowns
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
